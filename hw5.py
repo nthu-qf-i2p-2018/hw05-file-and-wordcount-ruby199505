07bfd74e34172e9f8a18db33d9123a2fe8b552e1
102048141 <ruby199505@gmail.com>
2018-04-21 23:47:52 +0800

    update

--- hw5.py
+++ hw5.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-import ...
-///
+import txt
+txtfile = open("i_have_a_dream.txt, encoding = 'utf-8')
 
 def main(filename):
     # read file into lines
-    lines = ...
+    lines = txtfile.readlines()
 
     # declare a word list
     all_words = []
@@ -13,40 +13,43 @@ def main(filename):
     for line in lines:
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
+        words = line.split()
 
         # check the format of words and append it to "all_words" list
         for word in words:
             # then, remove (strip) unwanted punctuations from every word
             # "dream." => "dream"
-            word = ...
+            word = word.rstrip(string.punctuation)
             # check if word is not empty
             if word:
                 # append the word to "all_words" list
-                all_words...
+                all_words.append(word)
 
     # compute word count from all_words
-    counter = ...
+    counter = Counter(all_words)
 
     # dump to a csv file named "wordcount.csv":
     # word,count
     # a,12345
     # I,23456
     # ...
-    with open(...) as csv_file:
+    with open("wordcount.csv", "w") as csv_file:
         # create a csv writer from a file object (or descriptor)
-        writer = ...
+        writer = csv_file.write()
         # write table head
         writer.writerow(['word', 'count'])
         # write all (word, count) pair into the csv writer
-        writer.writerows(...)
+        writer.writerows(counter)
 
     # dump to a json file named "wordcount.json"
-    ...
+    with open("wordcount.json", "w") as json_file:
+        writer = json_file.write()
+        writer.writerow(['word', 'count'])
+        writer.writeroes(counter)
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
-
+    pickle.dump(counter, open("wordcount.pkl", 'wb'))
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
