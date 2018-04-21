cf89a9cc879c15ece936582b9443689ed8f78f00
102048141 <ruby199505@gmail.com>
2018-04-22 00:35:58 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -19,8 +19,7 @@ def main(filename):
         for word in words:
             # then, remove (strip) unwanted punctuations from every word
             # "dream." => "dream"
-            improt string
-            word = word.replace(string.punctuation, "")
+            word = word.strip(string.punctuation)
             # check if word is not empty
             if word:
                 # append the word to "all_words" list
